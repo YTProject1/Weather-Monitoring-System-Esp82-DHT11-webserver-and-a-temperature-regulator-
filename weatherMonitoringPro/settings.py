46090c5b2a7df0ee4a817c71bd650b30d9e45d2e
DaviddairoT <dairotemitopedavid@gmail.com>
2025-01-26 19:05:18 +0100

    settings

--- weatherMonitoringPro/settings.py
+++ weatherMonitoringPro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l5_@*+oc2phjz-xfps-#nu8mslp2r$197uznm*pu458$s7z2@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherMonitoringapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'weatherMonitoringPro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
